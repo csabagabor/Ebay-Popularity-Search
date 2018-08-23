d25d0b9c5bb8965ec367e36f6ea774d583cfe9e4
csaba97 <csabagabor97@google.com>
2018-08-23 09:36:21 +0300

    replace <li> during load

---
+++ content.js
@@ -0,0 +1,41 @@
+
+
+
+setTimeout(function(){
+    replaceListItems();
+}, 100);
+
+
+function replaceListItems(){
+	var lists = document.getElementsByTagName('ul');
+	var list;
+	for(var i = 0; i < lists.length; i++) {
+		
+		if(lists[i].className === "srp-results srp-grid clearfix"){
+			list = lists[i];
+			//alert("megvan");
+		}
+	   
+	}
+
+	var searchValue = "Sold";
+	var listItems = [];
+	for(var i = 0; i < list.childNodes.length; i++) {
+		console.log(list.childNodes[i].nodeName);
+		if(list.childNodes[i].nodeName == "LI"){
+			//alert(inputs[i].id + " "+inputs[0].id);
+			listItems.push(list.childNodes[i]);
+		}
+	   
+	}
+
+
+	for(var i = 0; i < listItems.length; i++) {
+		
+		if(listItems[i].innerHTML.indexOf(searchValue) > -1){
+			//alert(inputs[i].id + " "+inputs[0].id);
+			list.insertBefore(listItems[i], listItems[0]);
+		}
+	   
+	}
+}
