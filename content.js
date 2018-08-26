df7f88db5e7fc28e999e063732aad0cabe4d22bb
csaba97 <csabagabor97@google.com>
2018-08-26 17:19:22 +0300

    get number of sold items

--- content.js
+++ content.js
@@ -10,12 +10,12 @@ function replaceListItems(){
 	var lists = document.getElementsByTagName('ul');
 	var list;
 	for(var i = 0; i < lists.length; i++) {
-		
+
 		if(lists[i].className === "gv-ic full-width left"){
 			list = lists[i];
 			//alert("megvan");
 		}
-	   
+
 	}
 
 	var searchValue = "Sold";
@@ -26,24 +26,34 @@ function replaceListItems(){
 			//alert(inputs[i].id + " "+inputs[0].id);
 			listItems.push(list.childNodes[i]);
 		}
-	   
+
 	}
 
 
 	for(var i = 0; i < listItems.length; i++) {
-		
-		if(listItems[i].innerHTML.indexOf(searchValue) > -1){
-			
-			listItems[i].childNodes.forEach(function(value){
-			  if(value.nodeType === Node.TEXT_NODE) { 
-				if(value.nodeValue.trim().indexOf(searchValue) > -1){
-				  console.log("Current textNode value is : ", value.nodeValue.trim())
-				}
-			  }
-			});	
-			//alert(inputs[i].id + " "+inputs[0].id);
-			list.insertBefore(listItems[i], listItems[0]);
+    var currItem = listItems[i];
+    var descendants = getDescendants(currItem);
+    	for(var j = 0; j < descendants.length; j++) {
+        if(descendants[j].nodeType == 3){
+          if(descendants[j].nodeValue.indexOf(searchValue) > -1){
+              alert(descendants[j].nodeValue.trim());
+          }
+        }
+      }
+
+
+			list.insertBefore(currItem, listItems[0]);
 		}
-	   
+
 	}
-}
+
+
+  function getDescendants(node, accum) {
+      var i;
+      accum = accum || [];
+      for (i = 0; i < node.childNodes.length; i++) {
+          accum.push(node.childNodes[i])
+          getDescendants(node.childNodes[i], accum);
+      }
+      return accum;
+  }
