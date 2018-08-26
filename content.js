63de854089c8b72715ae71f6886a46c72e061126
csaba97 <csabagabor97@google.com>
2018-08-26 18:26:06 +0300

    sort by popularity

--- content.js
+++ content.js
@@ -20,31 +20,54 @@ function replaceListItems(){
 
 	var searchValue = "Sold";
 	var listItems = [];
+
+
+
 	for(var i = 0; i < list.childNodes.length; i++) {
 		console.log(list.childNodes[i].nodeName);
 		if(list.childNodes[i].nodeName == "LI"){
 			//alert(inputs[i].id + " "+inputs[0].id);
 			listItems.push(list.childNodes[i]);
 		}
-
 	}
 
+  var hiddenLi = document.createElement("LI");
+  hiddenLi.style.display = "none";
+  list.insertBefore(hiddenLi, listItems[0]);
 
+
+  var listSortedItems = [];
 	for(var i = 0; i < listItems.length; i++) {
     var currItem = listItems[i];
     var descendants = getDescendants(currItem);
     	for(var j = 0; j < descendants.length; j++) {
         if(descendants[j].nodeType == 3){
           if(descendants[j].nodeValue.indexOf(searchValue) > -1){
-              alert(descendants[j].nodeValue.trim());
+              var text = descendants[j].nodeValue.trim();
+              var soldNr;
+              if(text.indexOf("+") > -1){//eg "37+ Sold"
+                soldNr = text.substring(0, text.indexOf(searchValue)-2);
+              }
+              else soldNr = text.substring(0, text.indexOf(searchValue)-1);
+
+              var listItemWithSoldNumber = {
+                  listItem : currItem,
+                  soldNumber : soldNr
+              }
+              listSortedItems.push(listItemWithSoldNumber);
           }
         }
       }
-
-
-			list.insertBefore(currItem, listItems[0]);
 		}
+      listSortedItems.sort(function(a, b){//descending order
+        return b.soldNumber - a.soldNumber;
+      })
 
+      for(var j = 0; j < listSortedItems.length; j++) {
+        // if listItems[0] is inserted before itself, problems occur
+        //that's why we are using a dummy element(hiddenLi) for first element
+        list.insertBefore(listSortedItems[j].listItem, hiddenLi);
+      }
 	}
 
 
